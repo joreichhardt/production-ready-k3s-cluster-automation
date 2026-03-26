9bb3b1d180ad1187266ef385e84c270b126396d6
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-03-26 10:34:10 -0500

    added tf vars

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -24,3 +24,17 @@ variable "image_name" {
   type    = string
   default = "debian13-golden-v1"
 }
+
+variable "vpc_name" {
+  type        = string
+  default     = "k3s-vpc"
+}
+
+variable "subnet_name" {
+  type        = string
+  default     = "k3s-subnet"
+}
+
+variable "subnet_cidr" {
+  type        = string
+  default     = "10.0.0.0/24"
