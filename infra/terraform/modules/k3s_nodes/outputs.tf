1ed057145a19b136f3bf0efaac4dbbcc2b6d7f1c
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-03-26 21:58:11 -0500

    ansible

--- infra/terraform/modules/k3s_nodes/outputs.tf
+++ infra/terraform/modules/k3s_nodes/outputs.tf
@@ -1,17 +1,3 @@
-output "instance_names" {
-  value = [for i in google_compute_instance.nodes : i.name]
-}
-
-output "instance_self_links" {
-  value = [for i in google_compute_instance.nodes : i.self_link]
-}
-
-output "private_ips" {
-  value = {
-    for k, v in google_compute_instance.nodes : k => v.network_interface[0].network_ip
-  }
-}
-
 output "nodes" {
   value = [
     for node in google_compute_instance.nodes : {
