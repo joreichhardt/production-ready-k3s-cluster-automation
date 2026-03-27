226591e55e9ef2b82a425bd477f745a623829bc0
Johannes Reichhardt <johannes.reichhardt@gmail.com>
2026-03-26 20:59:25 -0500

    output for ansible

--- infra/terraform/modules/k3s_nodes/outputs.tf
+++ infra/terraform/modules/k3s_nodes/outputs.tf
@@ -11,3 +11,13 @@ output "private_ips" {
     for k, v in google_compute_instance.nodes : k => v.network_interface[0].network_ip
   }
 }
+
+output "nodes" {
+  value = [
+    for idx, node in google_compute_instance.nodes : {
+      name       = node.name
+      private_ip = node.network_interface[0].network_ip
+      role       = idx == 0 ? "init" : "join"
+    }
+  ]
+}
